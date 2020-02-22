7df8f07bd9bc97bcee47f869bb36b07ec2f8ce40
victo <victo@host.docker.internal>
2020-02-22 00:24:40 -0300

    base

--- src/com/Main.java
+++ src/com/Main.java
@@ -1,9 +1,38 @@
 package com;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("hello toys");
 	}
 
+	public ArrayList<String> popularToys(int numToys, int topToys, List<String> toys, int numQuotes,
+			List<String> quotes) {
+
+		HashMap<String, Integer> totalQuotesByToys = new HashMap<>();
+
+		for (String quote : quotes) {
+			for (String toy : toys) {
+				if (quote.contains(toy)) {
+					if (totalQuotesByToys.get(toy) != null) {
+						totalQuotesByToys.put(toy, totalQuotesByToys.get(toy) + 1);
+					} else {
+						totalQuotesByToys.put(toy, 1);
+					}
+				}
+			}
+		}
+
+		ArrayList<String> topToysList = (ArrayList<String>) totalQuotesByToys.entrySet().stream()
+				.sorted(Comparator.comparing(Map.Entry::getValue)).map(Map.Entry::getKey).collect(Collectors.toList());
+
+		return topToysList;
+	}
 }
