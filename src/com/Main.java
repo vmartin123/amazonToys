16d65694c7eae010941f64a16a25a108aba827dc
victo <victo@host.docker.internal>
2020-02-25 15:12:09 -0300

    data

--- src/com/Main.java
+++ src/com/Main.java
@@ -10,7 +10,23 @@ import java.util.stream.Collectors;
 public class Main {
 
 	public static void main(String[] args) {
-		popularToys(5, 2, new ArrayList<String>(), 7, new ArrayList<String>());
+		ArrayList<String> toys = new ArrayList<>();
+		toys.add("elmo");
+		toys.add("elsa");
+		toys.add("legos");
+		toys.add("drone");
+		toys.add("tablet");
+		toys.add("warcraft");
+
+		ArrayList<String> quotes = new ArrayList<>();
+		quotes.add("Elmo is the hottest toy of the season! Elmo will be on every kid's wishlist!");
+		quotes.add("The new Elmo dolls are super high quality");
+		quotes.add("Expect the elsa dolls to be very popular this year");
+		quotes.add("Elsa and Elmo are the toys i'll be buying for my kids");
+		quotes.add("For parents of older kids, look into buying them a drone");
+		quotes.add("Warcraft is slowly rising in popularity ahead of the holiday season");
+
+		System.out.println("toys:" + popularToys(6, 2, toys, 6, quotes).toString());
 	}
 
 	public static ArrayList<String> popularToys(int numToys, int topToys, List<String> toys, int numQuotes,
